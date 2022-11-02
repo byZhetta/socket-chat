f215208fee35fd169b3d7b536a56128c8451bb30
ignaciozhetta <zagu4988@gmail.com>
2022-11-02 10:49:56 -0300

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,151 @@
+const url = ( window.location.hostname.includes('localhost') )
+            ? 'http://localhost:8080/api/auth/'
+            : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+
+let usuario = null;
+let socket = null;
+
+// Referencia HTML
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const ulUsuarios = document.querySelector('#ulUsuarios');
+const ulMensajes = document.querySelector('#ulMensajes');
+const btnSalir = document.querySelector('#btnSalir');
+
+const validarJWT = async() => {
+
+    const token = localStorage.getItem('token') || '';
+
+    if ( token.length <= 10 ) {
+        window.location = 'index.html';
+        throw new Error('No hay token en el servidor');
+    }
+
+    const resp = await fetch( url, {
+        headers: { 'x-token': token }
+    });
+
+    const { usuario: userDB, token: tokenDB } = await resp.json();
+    localStorage.setItem('token', tokenDB);
+    usuario = userDB;
+    document.title = usuario.nombre;
+    
+    await conectarSocket();
+}
+
+const conectarSocket = async() => {
+
+    socket = io({
+        'extraHeaders': {
+            'x-token': localStorage.getItem('token')
+        }
+    });
+
+    socket.on('connect', () => {
+        console.log('Sockets online')
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Sockets offline')
+    });
+    
+    socket.on('recibir-mensajes', dibujarMensajes);
+
+    socket.on('usuarios-activos', dibujarUsuarios);
+
+    // socket.on('mensaje-privado', (payload) => {
+    //     console.log('Privado: ', payload.mensaje)
+    // });
+
+    socket.on('mensaje-privado', (payload) => {
+        let mensajeprHtml = '';
+        // payload.forEach( ({ usuario, mensaje }) => {
+            mensajeprHtml += `
+                <li class="list-group-item">
+                    <p>
+                        <span class="text-success">[Privado]</span>
+                        <span class="text-warning">${ usuario.nombre }: </span>
+                        <span>${ payload.mensaje }</span>
+                    </p>
+                </li>
+            `;
+        // });
+        ulMensajes.innerHTML = mensajeprHtml;
+    });
+}
+
+const dibujarUsuarios = ( usuarios = [] ) => {
+
+    let usersHtml = '';
+    usuarios.forEach( ({ nombre, uid }) => {
+        usersHtml += `
+            <li class="list-group-item list-group-item-info">
+                <p>
+                    <h5 class="text-success">${ nombre }</h5>
+                    <span class="fs-6 text-muted">ID: ${ uid }</span>
+                </p>
+            </li>
+        `;
+    });
+    ulUsuarios.innerHTML = usersHtml;
+    
+}
+
+const dibujarMensajes = ( mensajes = [] ) => {
+
+    let mensajesHtml = '';
+    mensajes.forEach( ({ nombre, mensaje }) => {
+        mensajesHtml += `
+            <li class="list-group-item">
+                <p>
+                    <span class="text-primary">${ nombre }: </span>
+                    <span>${ mensaje }</span>
+                </p>
+            </li>
+        `;
+    });
+    ulMensajes.innerHTML = mensajesHtml;
+    
+}
+
+txtMensaje.addEventListener('keyup', ({ keyCode }) => {
+
+    const mensaje = txtMensaje.value;
+    const uid = txtUid.value;
+
+    if ( keyCode !== 13 ){ return; }
+    if ( mensaje.length === 0 ){ return; }
+
+    socket.emit('enviar-mensaje', { mensaje, uid });
+    txtMensaje.value = '';
+
+})
+
+btnSalir.addEventListener('click', () => {
+
+    localStorage.removeItem('token');
+
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then( () => {
+        console.log('User signed out.');
+        window.location = 'index.html';
+    });
+
+})
+
+const main = async() => {
+
+    // Validar JWT
+    await validarJWT();
+
+}
+
+// (()=>{
+//     gapi.load('auth2', () => {
+//         gapi.auth2.init();
+//         main();
+//     });
+// })();
+
+
+main();
